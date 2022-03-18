d65842a8ae1b9b15440bb092f48b8f64242d7acd
liuweilong <liuweilong.1014@bytedance.com>
2022-03-18 17:52:53 +0800

    调试成功

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+import config
+import json
 
 app = Flask(__name__)
 
+app.config.from_object(config)
+
+
+#api接口前缀
+apiPrefix = '/api/v1/'
+
+
+@app.route(apiPrefix + 'updateStaff', methods=['POST'])
+def updateStaff():
+    data = request.get_data(as_text=True)
+    # re = DBUtil.addOrUpdateStaff(data)
+    # if re['code'] >= 0: # 数据保存成功，移动附件
+    #     FileUtil.fileMoveDir(re['id'])
+    re ={"code":0}
+    return json.dumps(re)
+    # return 'Hello World!'
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello World!！'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
